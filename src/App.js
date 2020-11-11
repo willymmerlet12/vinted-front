17a8535e7cd82d7749ceae115b767fc56792be7e
Willy <willymmerlet@gmail.com>
2020-11-11 14:45:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Offers from "./containers/Offers";
+import Home from "./containers/Home";
 
 function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://vinted-will.herokuapp.com/");
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/items">
+          <Offers />
+        </Route>
+        <Route>
+          <Home path="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>
+        Home
+        <Link to="/items">Offer</Link>
+      </h1>
+    </div>
+  );
+};
+
+export default Home;

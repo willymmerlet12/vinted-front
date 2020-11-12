51382314a683bdb21202b4d84477c92a1dc0901c
Willy <willymmerlet@gmail.com>
2020-11-12 19:34:49 +0100

    home updated

--- src/components/Dis.js
+++ src/components/Dis.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Hero from "../img/vint.jpg";
+import { Link } from "react-router-dom";
 
 const Dis = () => {
   return (
     <>
       <div className="home-hero-bg">
         <img src={Hero} alt="" className="home-hero-forme" />
+        <div>
+          <div className="home-hero-pret">
+            Prêts à faire du tri dans vos placards ?
+            <Link to="/signup">
+              <button>Commencer à vendre</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
